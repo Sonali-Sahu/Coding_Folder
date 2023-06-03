c9ebe832ca303313e88eb5f89a2a867f90eae833
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-06-03 15:58:22 +0530

    Create Q4.py

---
+++ ppt6/Q4.py
@@ -0,0 +1,16 @@
+# Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        count = {0: -1}  # Initial count of 0's and 1's
+        max_len = count_diff = 0
+        
+        for i, num in enumerate(nums):
+            count_diff += 1 if num == 1 else -1  # Increment count of 1's or 0's
+            if count_diff in count:
+                max_len = max(max_len, i - count[count_diff])
+            else:
+                count[count_diff] = i
+        
+        return max_len
