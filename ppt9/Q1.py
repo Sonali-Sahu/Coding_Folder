93660faa2e026a0e910d70b0e96166e98391044e
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-06-03 21:43:36 +0530

    Create Q1.py

---
+++ ppt9/Q1.py
@@ -0,0 +1,6 @@
+# Given an integer `n`, return *`true` if it is a power of two. Otherwise, return `false`*.
+
+# An integer `n` is a power of two, if there exists an integer `x` such that `n == 2x`.
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return bin(n)[2:].rstrip('0') == '1'
