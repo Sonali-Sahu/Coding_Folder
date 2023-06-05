edb3f73469306282701415872636516c35222803
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-06-05 21:40:33 +0530

    Create Q1.py

---
+++ ppt10/Q1.py
@@ -0,0 +1,12 @@
+# Given an integer `n`, return *`true` if it is a power of three. Otherwise, return `false`*.
+
+# An integer `n` is a power of three, if there exists an integer `x` such that `n == 3x`.
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 0:
+            return False
+        if  n==1:
+            return True
+
+        return n%3==0 and self.isPowerOfThree(n/3)
