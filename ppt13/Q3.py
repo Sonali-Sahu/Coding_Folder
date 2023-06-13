72a6e647586eb5ae1d2fa6cf839f8eccb65c9036
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-06-13 20:06:07 +0530

    Create Q3.py

---
+++ ppt13/Q3.py
@@ -0,0 +1,66 @@
+# link list structure
+class Node:
+    def __init__(self):
+        self.data = 0
+        self.next = None
+
+
+# utility to create linklist node
+def newNode(val):
+    temp = Node()
+    temp.data = val
+    temp.next = None
+    return temp
+
+# utility to print a linklist
+def printLinkList(pointer):
+    while(pointer!=None):
+        print(pointer.data,"->",end=" ")
+        pointer = pointer.next
+    
+    print("NULL")
+
+
+def reverse( head, k):
+ 
+    if (head == None):
+        return head
+
+    q = []
+ 
+    current = head
+    i = 0
+    while (current != None) :
+        i = 1
+        while (i <= k) :
+            if (current == None):
+                break
+            q.append(current)
+            current = current.next
+            i = i + 1
+         
+        while (len(q) > 0):
+            front = q[-1]
+            last = q[0]
+             
+            temp = front.data
+            front.data = last.data
+            last.data = temp
+ 
+            if (len(q) > 0):
+                q.pop()
+
+            if (len(q)):
+                q.pop(0)
+     
+    return head
+ 
+head = newNode(5)
+head.next = newNode(5)
+head.next.next = newNode(17)
+head.next.next.next = newNode(15)
+head.next.next.next.next = newNode(11) 
+
+
+new_linklist = reverse(head,2)
+ 
