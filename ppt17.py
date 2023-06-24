edc8715fb101fcb7b07760b0517bc8ee7f4a56ff
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-06-24 10:15:55 +0530

    Update ppt17.py

--- ppt17.py
+++ ppt17.py
@@ -13,3 +13,24 @@ class Solution:
 
 
 
+#Q2.Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        maxSum,minSum,curMin,curMax,sum=(nums[0],nums[0],0,0,0)
+        for val in nums:
+            curMax=curMax+val if curMax+val>=val else val
+            maxSum=max(curMax,maxSum)
+            curMin=curMin+val if curMin+val<=val else val
+            minSum=min(minSum,curMin)
+            sum+=val
+        return maxSum if sum==minSum else max(maxSum,sum-minSum)
+
+
+#Q3
+
+
+
+
+
+
