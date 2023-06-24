f745330a77bcc16f18c74208e76aab1c44accc37
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-06-24 10:41:58 +0530

    Update ppt18.py

--- ppt18.py
+++ ppt18.py
@@ -43,7 +43,33 @@ class Solution:
 
 #Q2 Given an array nums with n objects colored red, white, or blue, sort them in-place so
 # that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
+        n = len(nums)
+        low = 0
+        high = n - 1
+        i = 0
+
+        while i <= high:
+            
+            if nums[i] == 0:
+                nums[i], nums[low] = nums[low], nums[i]
+                low += 1
+            
+            elif nums[i] == 2:
+                nums[i], nums[high] = nums[high], nums[i]
+                high -= 1
+                i -= 1
+            i += 1
+
+
+#Q3. You are a product manager and currently leading a team to develop a new product.
+# Unfortunately, the latest version of your product fails the quality check.
+# Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
 
 
