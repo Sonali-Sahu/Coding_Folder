34a059856416667e860b046ecfb54d90512653ca
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-06-24 10:17:59 +0530

    Update ppt17.py

--- ppt17.py
+++ ppt17.py
@@ -27,10 +27,22 @@ class Solution:
         return maxSum if sum==minSum else max(maxSum,sum-minSum)
 
 
-#Q3
+#Q3 The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively.
+# All students stand in a queue. Each student either prefers square or circular sandwiches.
 
 
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        while(len(students)!=0):
+            if(sandwiches[0] not in students):
+                return(len(students))
+            if(students[0]==sandwiches[0]):
+                students.pop(0)
+                sandwiches.pop(0)
+            else:
+                a=students.pop(0)
+                students.append(a)
+        return(len(students))
 
-
-
+# Q4. 
 
