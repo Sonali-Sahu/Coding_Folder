c42966fe1470022a5935f995e357e018b3e403c2
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-08-12 17:40:36 +0530

    Update 724. Find Pivot Index.py

--- LeetCode/DSA Sheet by FRAZ/724. Find Pivot Index.py
+++ LeetCode/DSA Sheet by FRAZ/724. Find Pivot Index.py
@@ -1,6 +1,7 @@
 https://leetcode.com/problems/find-pivot-index/description/
 
-Approach - List Comprehension - Time Complexity = O(n)
+# Approach - List Comprehension - Time Complexity = O(n), Space Complexity = O(1)
+# High Runtime as Sum is used in each loop 2 times
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         n =len(nums)
@@ -10,3 +11,16 @@ class Solution:
         return -1
 
   
+# Better Approach :- More Mathematical (+/-) than Using Sum So many Time in each loop
+# Time Complexity = O(n), Space Complexity = O(1)
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum,right_sum = 0,sum(nums)
+        for i, ele in enumerate(nums):
+            right_sum -= ele
+            if left_sum == right_sum:
+                return i
+            left_sum += ele
+        return -1
+            
