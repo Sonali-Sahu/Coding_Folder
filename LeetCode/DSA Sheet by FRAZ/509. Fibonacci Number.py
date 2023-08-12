2fe0cb9907bbadb19440f5df9dd56884c05000c4
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-08-12 21:51:24 +0530

    Update 509. Fibonacci Number.py

--- LeetCode/DSA Sheet by FRAZ/509. Fibonacci Number.py
+++ LeetCode/DSA Sheet by FRAZ/509. Fibonacci Number.py
@@ -8,3 +8,22 @@ class Solution:
             return n
         else:
             return self.fib(n-1) + self.fib(n-2)
+
+
+# time complexity - O(n) Linear
+# space complexity - O(n)
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<=1:
+            return n
+        a,b = 0 , 1
+        sum_fib = 0
+
+        while n>1:
+            sum_fib = a + b
+            a = b
+            b = sum_fib
+            n -= 1
+        
+        return sum_fib
