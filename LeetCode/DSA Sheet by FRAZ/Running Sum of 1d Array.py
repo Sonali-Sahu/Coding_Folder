4cd4c5100ded9ba05bfbefb637c6eb3e6c2385ce
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-08-12 17:06:08 +0530

    Update Running Sum of 1d Array.py

--- LeetCode/DSA Sheet by FRAZ/Running Sum of 1d Array.py
+++ LeetCode/DSA Sheet by FRAZ/Running Sum of 1d Array.py
@@ -11,3 +11,13 @@ class Solution:
             new_list.append(runn_sum)
         
         return new_list
+
+Best) Approach 2 - InPlace Calculation - Mathematics
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        # time complexity O(n) 
+        # space complexity O(1)
+        for i in range(1,len(nums)):
+            nums[i] = nums[i-1] + nums[i]
+
+        return nums
