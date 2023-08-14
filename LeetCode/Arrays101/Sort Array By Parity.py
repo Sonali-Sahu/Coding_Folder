1f873e3014499ed8d99323c44544bf2b57c2a837
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-08-14 11:25:47 +0530

    Update Sort Array By Parity.py

--- LeetCode/Arrays101/Sort Array By Parity.py
+++ LeetCode/Arrays101/Sort Array By Parity.py
@@ -9,3 +9,18 @@ Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accep
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         return [x for x in nums if x%2 == 0 ]+[x for x in nums if x%2 != 0]
+
+
+# TC = O(n)
+# SC = O(1)
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] %2 ==0:
+                nums[j],nums[i] = nums[i],nums[j]
+                j +=1
+            
+        return nums
+        
