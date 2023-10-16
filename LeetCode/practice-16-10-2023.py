4f9f5c6bdbc2ac2a82b7f20e43215a8f833e58e8
Sonali Sahu <48079283+Sonali-Sahu@users.noreply.github.com>
2023-10-16 11:41:24 +0530

    Update practice-16-10-2023.py

--- LeetCode/practice-16-10-2023.py
+++ LeetCode/practice-16-10-2023.py
@@ -17,4 +17,43 @@ if len(string2_list) == 0:
     print('True, Strings are anagram')
 else:
     print('False, Strings are not anagram')
-    
+
+
+# HashMap way 
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        countS,countT = {},{}
+        for ch in s:
+            if ch in countS:
+                countS[ch] += 1
+            else:
+                countS[ch] = 1
+
+        for ch in t:
+            if ch in countT:
+                countT[ch] += 1
+            else:
+                countT[ch] = 1
+        value_flag = 1
+
+        for ch_key,ch_value in countS.items():
+            if ch_key not in countT:
+                value_flag = 0
+            if  ch_key in countT and countT[ch_key] != ch_value:
+                value_flag = 0
+
+        if value_flag:
+            return True
+        else:
+            return False
+
+
+# sorted way
+
+
+
+        
